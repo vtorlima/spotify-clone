662287b720e38406c164d3430f2973fd21ac09ce
Vitor Lima dos Santos <vtorlima@hotmail.com>
2026-07-28 16:38:35 -0300

    feat: exibe banner de destaque e artistas recentes (em destaque) na home

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
+import { HomeHeroBanner } from "../components/home/HomeHeroBanner";
 import { HomePlaylistsShelf } from "../components/home/HomePlaylistsShelf";
 import { HomeRecentAlbumsShelf } from "../components/home/HomeRecentAlbumsShelf";
+import { HomeRecentArtistsShelf } from "../components/home/HomeRecentArtistsShelf";
 
 export default function HomePage() {
   return (
     <section className="flex flex-col gap-8 p-6">
+      <HomeHeroBanner />
       <HomePlaylistsShelf />
       <HomeRecentAlbumsShelf />
+      <HomeRecentArtistsShelf />
     </section>
   );
 }
