c95d02d9878fda2c235425ebde2804da70735206
Vitor Lima dos Santos <vtorlima@hotmail.com>
2026-07-28 16:02:26 -0300

    feat: exibe albuns recentes na home (como albuns em destaque)

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { HomePlaylistsShelf } from "../components/home/HomePlaylistsShelf";
+import { HomeRecentAlbumsShelf } from "../components/home/HomeRecentAlbumsShelf";
 
 export default function HomePage() {
   return (
     <section className="flex flex-col gap-8 p-6">
       <HomePlaylistsShelf />
+      <HomeRecentAlbumsShelf />
     </section>
   );
 }
